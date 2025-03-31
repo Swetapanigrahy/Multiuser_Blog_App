acbc1d0c5b3a7b847d79f93c6808e9c25eb158eb
Sweta panigrahy <152600276+Swetapanigrahy@users.noreply.github.com>
2025-03-31 12:51:57 +0530

    Update OAuth.jsx

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -18,7 +18,7 @@ export default function OAuth() {
         provider.setCustomParameters({ prompt: 'select_account' })
         try {
             const resultsFromGoogle = await signInWithPopup(auth, provider)
-            const res = await fetch('https://blog-app-five-inky.vercel.app/', {
+            const res = await fetch('/api/auth/google', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
